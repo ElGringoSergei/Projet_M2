43a6c621da136f0be6e7af76cbe0e89ec5aceab2
ElGringoSergei <matheo.wartel@icloud.com>
2023-10-20 10:07:15 +0200

    s

--- public_html/sign_in.php
+++ public_html/sign_in.php
@@ -51,7 +51,7 @@ session_start();
                 </div>
                 <label for="username" class="form-label bold">Identifiant de connexion</label>
                 <input type="text" pattern="[a-zA-Z][0-9]*" class="form-control" id="username" name="username" oninput="checkBlankSpace()" placeholder="jdoe">
-                <label for="username" class="form-text">Les espaces et les caractères spéciaux ne sont pas autorisés</label><br>
+                <label for="username" class="form-text" style="margin-left: 0.2rem">Les espaces et les caractères spéciaux ne sont pas autorisés</label><br>
                 <label for="exampleInputEmail1" class="form-label bold">Adresse mail</label>
                 <input type="email" class="form-control" id="Email1" aria-describedby="emailHelp" name="Email1" placeholder="johndoe@mail.com">
                 <div id="emailHelp" class="form-text" style="margin-left: 0.2rem;">Nous ne divulgerons pas votre adresse mail</div>
