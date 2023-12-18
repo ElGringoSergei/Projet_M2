a98842f341955339ba878b86848303ae644cb5a7
ElGringoSergei <matheo.wartel@icloud.com>
2023-12-18 15:43:58 +0100

    Update for working shell

--- public_html/shell.php
+++ public_html/shell.php
@@ -13,25 +13,38 @@ if (isset($_SESSION['id'])) {
 } else {
     header("Location: ../login.php?error=Session expirée");
 }
-$token_del = bin2hex(random_bytes(16));
-$_SESSION['csrf_del'] = $token_del;
 $token = bin2hex(random_bytes(16));
 $_SESSION['csrf'] = $token;
 if(isset($_POST['command'])) {
-    $post_data = http_build_query(
+    $postdata = http_build_query(
         array(
             'command' => $_POST['command']
         )
     );
-    $options = array('https' =>
-        array(
-            'method' => 'POST',
-            'header' => 'Content-type: application/x-www-form-urlencoded',
-            'content' => $postdata
-        )
+
+    $url = 'https://10.5.0.4/run_command';
+
+    $ch = curl_init($url);
+
+    $options = array(
+        CURLOPT_POST => 1,
+        CURLOPT_POSTFIELDS => $postdata,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_SSL_VERIFYHOST => 0, // N'utilisez pas en production, c'est pour les tests
+        CURLOPT_SSL_VERIFYPEER => 0  // N'utilisez pas en production, c'est pour les tests
     );
-    $context = stream_context_create($options);
-    $text_content = file_get_contents("https://10.5.0.4/run_command", false, $context);
+
+    curl_setopt_array($ch, $options);
+
+    $response = curl_exec($ch);
+
+    if ($response === false) {
+        echo 'Erreur cURL : ' . curl_error($ch);
+    } else {
+        $text_content = json_decode($response, true);
+    }
+
+    curl_close($ch);
 } else {
     $text_content = '';
 }
@@ -46,9 +59,9 @@ if(isset($_POST['command'])) {
     <title>Remote Shell</title>
 </head>
 <body>
-    <textarea id="output" rows="10" cols="50" readonly><?php echo $text_content; ?></textarea>
-    <form method="post" action="">
-    	<input type="text" id="command" placeholder="Entrer une commande">
+    <textarea id="output" rows="10" cols="50" readonly><?php echo $text_content['output']; ?></textarea>
+    <form method="post" action="#">
+    	<input type="text" id="command" name="command" placeholder="Entrer une commande">
     	<button type="submit">Envoyer la commande</button>
     </form>
 </body>
--- python/home/distant_shell.py
+++ python/home/distant_shell.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 # Route pour exécuter une commande shell
 @app.route('/run_command', methods=['POST'])
 def run_command():
+    print('Requete recue\n')
     command = request.form['command']
     
     # Utilise subprocess pour exécuter la commande
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
     # Retourne la sortie de la commande
-    return jsonify({'output': result.stdout, 'error': result.stderr})
+    return {"error": result.stderr, "output": result.stdout}
 
 # Si c'est le fichier principal, lance l'application avec Gunicorn
 if __name__ == '__main__':
